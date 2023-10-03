21d46e2f7115fadba9783d0ea846e3ebef9fbf7a
Lothar "Husky110" Hoffmann <l.hoffmann@cherrymint.de>
2023-10-03 16:06:53 +0200

    install-script

--- install.sh
+++ install.sh
@@ -2,13 +2,13 @@
 
 if [ ! -f /usr/bin/docker-volume-local-persist ]; then
   echo "Installing local-persist driver to docker (will ask you for your root-password!)..."
-  wget https://github.com/MatchbookLab/local-persist/releases/download/v1.3.0/local-persist-linux-amd64
-  sudo chmod +x local-persist-linux-amd64
-  sudo mv local-persist-linux-amd64 /usr/bin/docker-volume-local-persist
+  wget -O docker-volume-local-persist https://github.com/MatchbookLab/local-persist/releases/download/v1.3.0/local-persist-linux-amd64
+  sudo chmod +x docker-volume-local-persist
+  sudo mv docker-volume-local-persist /usr/bin/docker-volume-local-persist
 fi
 if [ ! -f /etc/systemd/docker-volume-local-persist.service ]; then
-  wget https://github.com/MatchbookLab/local-persist/blob/master/init/systemd.service
-  sudo mv systemd.service /etc/systemd/docker-volume-local-persist.service
+  wget -O docker-volume-local-persist.service https://github.com/MatchbookLab/local-persist/blob/master/init/systemd.service
+  sudo mv docker-volume-local-persist.service /etc/systemd/docker-volume-local-persist.service
   sudo systemctl daemon-reload
   sudo systemctl enable docker-volume-local-persist
   sudo systemctl start docker-volume-local-persist
