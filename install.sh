40d62b07d0128518790fd14eddc899c1c21c8874
Lothar "Husky110" Hoffmann <l.hoffmann@cherrymint.de>
2023-10-03 16:04:54 +0200

    install-script

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-if[ ! -f /usr/bin/docker-volume-local-persist ]; then
+if [ ! -f /usr/bin/docker-volume-local-persist ]; then
   echo "Installing local-persist driver to docker (will ask you for your root-password!)..."
   wget https://github.com/MatchbookLab/local-persist/releases/download/v1.3.0/local-persist-linux-amd64
   sudo chmod +x local-persist-linux-amd64
   sudo mv local-persist-linux-amd64 /usr/bin/docker-volume-local-persist
 fi
-if[ ! -f /etc/systemd/docker-volume-local-persist.service ]; then
+if [ ! -f /etc/systemd/docker-volume-local-persist.service ]; then
   wget https://github.com/MatchbookLab/local-persist/blob/master/init/systemd.service
   sudo mv systemd.service /etc/systemd/docker-volume-local-persist.service
   sudo systemctl daemon-reload
