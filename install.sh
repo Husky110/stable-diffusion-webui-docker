e2937670a54cf524bbd75b41ac7b6b8997129b7b
Lothar "Husky110" Hoffmann <l.hoffmann@cherrymint.de>
2023-10-03 16:13:42 +0200

    install-script

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ if [ ! -f /usr/bin/docker-volume-local-persist ]; then
 fi
 if [ ! -f /etc/systemd/docker-volume-local-persist.service ]; then
   wget -O docker-volume-local-persist.service https://raw.githubusercontent.com/MatchbookLab/local-persist/master/init/systemd.service
-  sudo mv docker-volume-local-persist.service /etc/systemd/docker-volume-local-persist.service
+  sudo mv docker-volume-local-persist.service /etc/systemd/system/docker-volume-local-persist.service
   sudo systemctl daemon-reload
   sudo systemctl enable docker-volume-local-persist
   sudo systemctl start docker-volume-local-persist
