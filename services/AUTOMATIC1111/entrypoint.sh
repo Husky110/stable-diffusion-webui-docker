0c192e25f8ffbc175f2d7b701d15cd18c930af61
Lothar "Husky110" Hoffmann <l.hoffmann@cherrymint.de>
2023-10-04 15:18:19 +0200

    A1111-Init

--- services/AUTOMATIC1111/entrypoint.sh
+++ services/AUTOMATIC1111/entrypoint.sh
@@ -3,55 +3,51 @@
 set -Eeuo pipefail
 
 # Set up config file
-#python /docker/config.py /data/auto/config/config.json <- I'm leaving config.py intact in case we need it again later
+#python /docker/config.py /data/configs/config.json <- I'm leaving config.py intact in case we need it again later
 
-if [ ! -f /data/auto/config/ui-config.json ]; then
-  echo '{}' > /data/auto/config/ui-config.json
+if [ ! -f /data/configs/ui-config.json ]; then
+  echo '{}' > /data/configs/ui-config.json
 fi
 
-if [ ! -f /data/auto/config/styles.csv ]; then
-  touch /data/auto/config/styles.csv
+if [ ! -f /data/configs/styles.csv ]; then
+  touch /data/configs/styles.csv
+fi
+
+if [ ! -f /data/configs/config.json ]; then
+  cp /data/configs/repo-default-config.json /data/configs/config.json
 fi
 
 #running initalization
-if [ ! -f /data/auto/docker-init-complete ]; then
+if [ ! -f /data/configs/docker-init-complete ]; then
   echo "Initializing..."
-  cp /docker/default-config.json /data/auto/config/config.json
   cd ${ROOT}
-  #initialize a symlink for the embeddings
-  rm -rf embeddings && ln -s /data/models/Embeddings embeddings
 
-  # extensions...
-  if [ ! -d /data/auto/extensions ]; then
-      mkdir /data/auto/extensions
-  fi
-  rm -rf extensions && ln -s /data/auto/extensions extensions
   #adding recommended extensions
-  if [ ! -d /data/auto/extensions/sd-webui-controlnet ]; then
-        cp -rf ${ROOT}/repositories/sd-webui-controlnet /data/auto/extensions/sd-webui-controlnet
+  if [ "$A1111_INSTALL_RECOMMENDED_EXTENSION_CONTROLNET" == "1" ]
+  then
+    if [ ! -d /data/auto/extensions/sd-webui-controlnet ]; then
+          cp -rf ${ROOT}/repositories/sd-webui-controlnet /data/auto/extensions/sd-webui-controlnet
+    fi
   fi
-  if [ ! -d /data/auto/extensions/multidiffusion-upscaler-for-automatic1111 ]; then
-      cp -rf ${ROOT}/repositories/multidiffusion-upscaler-for-automatic1111 /data/auto/extensions/multidiffusion-upscaler-for-automatic1111
+  if [ "$A1111_INSTALL_RECOMMENDED_EXTENSION_CONTROLNET" == "1" ]
+    then
+    if [ ! -d /data/auto/extensions/multidiffusion-upscaler-for-automatic1111 ]; then
+        cp -rf ${ROOT}/repositories/multidiffusion-upscaler-for-automatic1111 /data/auto/extensions/multidiffusion-upscaler-for-automatic1111
+    fi
   fi
-  if [ ! -d /data/auto/extensions/sd-webui-regional-prompter ]; then
-      cp -rf ${ROOT}/repositories/sd-webui-regional-prompter /data/auto/extensions/sd-webui-regional-prompter
+  if [ "$A1111_INSTALL_RECOMMENDED_EXTENSION_CONTROLNET" == "1" ]
+    then
+    if [ ! -d /data/auto/extensions/sd-webui-regional-prompter ]; then
+        cp -rf ${ROOT}/repositories/sd-webui-regional-prompter /data/auto/extensions/sd-webui-regional-prompter
+    fi
   fi
 
-  ln -s /data/auto/config/config.json config.json
-  ln -s /data/auto/config/ui-config.json ui-config.json
-  ln -s /data/auto/config/styles.csv styles.csv
+  ln -s /data/configs/config.json config.json
+  ln -s /data/configs/ui-config.json ui-config.json
+  ln -s /data/configs/styles.csv styles.csv
 
-  #Codeformer
-  if [ ! -d ${ROOT}/repositories/CodeFormer/weights/facelib ]; then
-    mkdir -p ${ROOT}/repositories/CodeFormer/weights/facelib
-  else
-    rm -rf /codeformer_facelib/*
-    mv -v ${ROOT}/repositories/CodeFormer/weights/facelib /codeformer_facelib
-    rm -rf ${ROOT}/repositories/CodeFormer/weights/facelib
-  fi
-  ln -s /codeformer_facelib ${ROOT}/repositories/CodeFormer/weights/facelib
   #finalizing
-  touch /data/auto/docker-init-complete
+  touch /data/configs/docker-init-complete
 fi
 
 echo "Installing extension dependencies (if any)"
@@ -69,10 +65,10 @@ for installscript in "${list[@]}"; do
   PYTHONPATH=${ROOT} python "$installscript"
 done
 
-if [ -f "/data/auto/config/startup.sh" ]; then
+if [ -f "/data/configs/startup.sh" ]; then
   pushd ${ROOT}
   echo "Running startup script"
-  . /data/auto/config/startup.sh
+  . /data/configs/startup.sh
   popd
 fi
 rm -rf /output/temp/auto/*.*
